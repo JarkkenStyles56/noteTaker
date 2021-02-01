a7c87074e55022117f183006be9d49bc7bc9cb0d
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2021-02-01 13:51:53 -0700

    installed all necessary packages and fleshed out routes

---
+++ db/store.js
@@ -0,0 +1,57 @@
+const util = require("util");
+const fs = require("fs");
+
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  constructor(_dbPath){
+    this.dbPath = _dbPath;
+  }
+  read() {
+    return readFileAsync(this.dbPath, "utf8").catch(err => console.error(err));
+  }
+
+  write(notes) {
+    return writeFileAsync(this.dbPath, JSON.stringify(notes)).catch(err => console.error(err));;
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+
+    const newNote = { title, text, id: uuidv1() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+
+  removeNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
+}
+
+module.exports = Store;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,24 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+    store
+      .getNotes()
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  router.post("/notes", (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  router.delete("/notes/:id", (req, res) => {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+  module.exports = router;
